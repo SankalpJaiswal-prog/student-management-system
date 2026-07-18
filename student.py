a095e7c7a07f9a51f97370650edb33969538af00
SankalpJaiswal-prog <sankalp.jaiswal@sdbi.in>
2026-07-18 17:35:41 +0530

    Fixed GitHub Actions workflow

--- student.py
+++ student.py
@@ -4,6 +4,8 @@ students = {}
 def add_student(student_id, name):
     if student_id in students:
         return False
+    students[student_id] = name
+    return True
 
 
 def remove_student(student_id):
